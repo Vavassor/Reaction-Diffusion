23bf6fbe75d67b7b27ab6bf6a23beaabe9b9f61c
Andrew Dawson <dawso.andrew@gmail.com>
2019-06-23 21:26:31 -0400

    Reintroduces wrapping behavior in shaders.

--- app/Source/Shaders/advect-fs.glsl
+++ app/Source/Shaders/advect-fs.glsl
@@ -9,16 +9,23 @@ uniform sampler2D velocity_field;
 
 varying vec2 surface_texcoord;
 
+// WebGL 1.0 doesn't support use of the wrapping parameter GL_REPEAT for
+// non-power-of-two texture sizes. This manually implements that feature.
+vec4 sample_wrap(sampler2D input_sampler, vec2 texcoord)
+{
+    return texture2D(input_sampler, fract(texcoord));
+}
+
 vec4 sample_bilinear(sampler2D source, vec2 texel_size, vec2 sample_position)
 {
     vec2 integral_part = floor(sample_position - 0.5) + 0.5;
     vec2 fractional_part = sample_position - integral_part;
     vec2 texcoord = texel_size * integral_part;
 
-    vec4 southwest = texture2D(source, texcoord);
-    vec4 northwest = texture2D(source, vec2(texcoord.x, texcoord.y + texel_size.y));
-    vec4 southeast = texture2D(source, vec2(texcoord.x + texel_size.x, texcoord.y));
-    vec4 northeast = texture2D(source, texcoord + texel_size);
+    vec4 southwest = sample_wrap(source, texcoord);
+    vec4 northwest = sample_wrap(source, vec2(texcoord.x, texcoord.y + texel_size.y));
+    vec4 southeast = sample_wrap(source, vec2(texcoord.x + texel_size.x, texcoord.y));
+    vec4 northeast = sample_wrap(source, texcoord + texel_size);
 
     vec4 north = mix(northwest, northeast, fractional_part.x);
     vec4 south = mix(southwest, southeast, fractional_part.x);
--- app/Source/Shaders/pressure-fs.glsl
+++ app/Source/Shaders/pressure-fs.glsl
@@ -8,14 +8,21 @@ uniform vec2 field_size;
 
 varying vec2 surface_texcoord;
 
+// WebGL 1.0 doesn't support use of the wrapping parameter GL_REPEAT for
+// non-power-of-two texture sizes. This manually implements that feature.
+vec4 sample_wrap(sampler2D input_sampler, vec2 texcoord)
+{
+    return texture2D(input_sampler, fract(texcoord));
+}
+
 void main()
 {
     vec2 pixel_size = 1.0 / field_size;
     float divergence = texture2D(divergence_field, surface_texcoord).x;
-    float west = texture2D(pressure_field, surface_texcoord - vec2(2.0 * pixel_size.x, 0.0)).x;
-    float east = texture2D(pressure_field, surface_texcoord + vec2(2.0 * pixel_size.x, 0.0)).x;
-    float south = texture2D(pressure_field, surface_texcoord - vec2(0.0, 2.0 * pixel_size.y)).x;
-    float north = texture2D(pressure_field, surface_texcoord + vec2(0.0, 2.0 * pixel_size.y)).x;
+    float west = sample_wrap(pressure_field, surface_texcoord - vec2(2.0 * pixel_size.x, 0.0)).x;
+    float east = sample_wrap(pressure_field, surface_texcoord + vec2(2.0 * pixel_size.x, 0.0)).x;
+    float south = sample_wrap(pressure_field, surface_texcoord - vec2(0.0, 2.0 * pixel_size.y)).x;
+    float north = sample_wrap(pressure_field, surface_texcoord + vec2(0.0, 2.0 * pixel_size.y)).x;
     float pressure = 0.25 * (divergence + west + east + south + north);
     gl_FragColor = vec4(pressure, 0.0, 0.0, 1.0);
 }
